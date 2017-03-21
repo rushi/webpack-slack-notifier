ab5792db8c0d289c262ab020111cb27d6998ca5a
Rushi Vishavadia <rushi.v@gmail.com>
2017-03-21 20:17:32 +0400

    Show response when there is an error only

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const Slack = require('slack-node');
 
 function SlackPlugin(options) {
-  this.options = options;
-  this.options.channel = options.channel || '#general';
-  this.options.username = options.username || 'webhookbot';
-  this.options.icon_emoji = options.icon_emoji || ':ghost:';
+    this.options = options;
+    this.options.channel = options.channel || '#general';
+    this.options.username = options.username || 'webhookbot';
+    this.options.icon_emoji = options.icon_emoji || ':ghost:';
 }
 
-SlackPlugin.prototype.apply = function (compiler) {
-  const slack = new Slack();
-  slack.setWebhook(this.options.webhookUri);
+SlackPlugin.prototype.apply = function(compiler) {
+    const slack = new Slack();
+    slack.setWebhook(this.options.webhookUri);
 
-  var self = this;
-  compiler.plugin('done', function () {
-    console.log('Sending a slack message to', self.channel);
-    slack.webhook(self.options, function (err, response) {
-      if (err) { console.log('Error', err); }
-      console.log('Response', response);
+    var self = this;
+    compiler.plugin('done', function() {
+        console.log('Sending a slack message to', self.channel);
+        slack.webhook(self.options, function(err, response) {
+            if (err) {
+                console.log('Error', err);
+                console.log('Response', response);
+            } else {
+                console.log('Message successfully posted');
+            }
+        });
     });
-  });
 };
 
 module.exports = SlackPlugin;
